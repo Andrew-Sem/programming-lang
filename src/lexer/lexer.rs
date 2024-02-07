7f36c0d595a617cb3506e8e5910dc009ceb7faec
Andrew-Sem <avvyayvayva@gmail.com>
2024-02-07 16:23:30 +0300

    test: lexer

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -1,6 +1,7 @@
 use std::collections::{HashMap, VecDeque};
 
 #[derive(Debug, Clone, Copy)]
+#[derive(PartialEq)]
 enum TokenType{
     // literal types
     Null,
@@ -82,6 +83,7 @@ pub fn tokenize(source_code: &str) -> Vec<Token> {
                     num.push(digit);
                     src.pop_front();
                 }
+                tokens.push(Token::new(num, TokenType::Number));
             },
             // build letters token
             Some(c) if c.is_alphabetic() => {
@@ -100,7 +102,7 @@ pub fn tokenize(source_code: &str) -> Vec<Token> {
             },
             Some(c) if c.is_whitespace() => {
                 src.pop_front();
-                // Return unit type to match other arms
+                // return unit type to match other arms
                 ()
             },
             _ => {
@@ -111,3 +113,98 @@ pub fn tokenize(source_code: &str) -> Vec<Token> {
     tokens.push(Token::new(String::from("EndOfFile"), TokenType::EOF));
     tokens
 }
+
+#[cfg(test)]
+mod tests{
+    use crate::lexer::lexer::{tokenize, TokenType};
+
+    #[test]
+    fn should_analyze_simple_code() {
+        let source = "let x = 45 * (4 / 3)";
+        let tokens = tokenize(source);
+        let expected_len = 11; // code tokens + EOF
+        assert_eq!(
+            expected_len,
+            tokens.len(),
+            "{:?} should have len {} but was {}",
+            tokens,
+            expected_len,
+            tokens.len()
+        );
+    }
+
+    #[test]
+    fn should_analyze_some_keywords(){
+        let source = "null let";
+        let tokens = tokenize(source);
+        let expected_len = 3; // code tokens + EOF
+        assert_eq!(
+            expected_len,
+            tokens.len(),
+            "{:?} should have len {} but was {}",
+            tokens,
+            expected_len,
+            tokens.len()
+        );
+    }
+
+    #[test]
+    fn should_analyze_int_numbers(){
+        let source = "0 2 9 4567890";
+        let tokens = tokenize(source);
+        let expected_len = 5; // code tokens + EOF
+        assert_eq!(
+            expected_len,
+            tokens.len(),
+            "{:?} should have len {} but was {}",
+            tokens,
+            expected_len,
+            tokens.len()
+        );
+    }
+
+    #[test]
+    fn should_analyze_variable_names(){
+        let source = "x y z someVariable VARIABLE";
+        let tokens = tokenize(source);
+        let expected_len = 6; // code tokens + EOF
+        assert_eq!(
+            expected_len,
+            tokens.len(),
+            "{:?} should have len {} but was {}",
+            tokens,
+            expected_len,
+            tokens.len()
+        );
+    }
+
+    #[test]
+    fn should_ignore_whitespaces(){
+        let source = "\n\t  \r\n";
+        let tokens = tokenize(source);
+        let expected_len = 1; // EOF
+        assert_eq!(
+            expected_len,
+            tokens.len(),
+            "{:?} should only contain EOF",
+            tokens,
+        );
+    }
+
+    #[test]
+    fn should_analyze_some_operators(){
+        let source = "+ - * / %";
+        let tokens = tokenize(source);
+        let expected_len = 6; // code tokens + EOF
+        assert_eq!(
+            expected_len,
+            tokens.len(),
+            "{:?} should have len {} but was {}",
+            tokens,
+            expected_len,
+            tokens.len()
+        );
+    }
+}
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use crate::lexer::tokenize;
+
+use crate::lexer::lexer::tokenize;
 
 mod lexer;
-
 
 fn main() {
     let source_code = "let x = 45 * (4 / 3)";
